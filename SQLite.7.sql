61c4fb7d71e74c7c641a3ed7735eeaeeb500bb2d
Iasmine <Iasmine948@gmail.com>
2025-04-20 16:35:42 -0300

    UPDATES

---
+++ SQLite.7.sql
@@ -0,0 +1,3 @@
+UPDATE tabelapedidos set status = 'Enviado' WHERE status = 'Processando';
+
+SELECT * FROM tabelapedidos;
---
+++ SQLite.8.sql
@@ -0,0 +1,5 @@
+UPDATE tabelaclientes SET informacoes_de_contato = 'j.santos@email.com',
+endereço_cliente = 'rua dos caraioaquatro, 30'
+WHERE id_cliente = 2;
+
+SELECT * FROM tabelaclientes;
