cd321161a757344fe0b5aba70cf5445cfdf1887c
Iasmine <Iasmine948@gmail.com>
2025-04-20 16:01:05 -0300

    CREATE tabelas de produto e fornecedores

---
+++ SQLite.3.sql
@@ -0,0 +1,8 @@
+INSERT INTO tabelaclientes
+(id_cliente,
+ nome_cliente,
+ informacoes_de_contato,
+ Endereço_Cliente)
+ VALUES
+(5,’Patrícia Lima’,’patricia.lima@email.com’,’Rua das Flores, 123’),
+(6,’Rodrigo Almeida’,’rodrigo.almeida@email.com’,’Avenida Central, 456’);
---
+++ SQLite.4.sql
@@ -0,0 +1,13 @@
+INSERT INTO tabelaprodutos (
+  ID_Produto,
+  Nome_do_Produto,
+  Descrição,
+  Categoria,
+  Preco_de_Compra,
+  Unidade,
+  Fornecedor,
+  Data_de_Inclusao)
+VALUES
+(1, 'Smartphone X', 'Smartphone de última geração', 1, 699.99, 'Unidade', 1, '2023-08-01'),
+(2, 'Notebook Pro', 'Notebook poderoso com tela HD', 2, 1199.99, 'Unidade', 2, '2023-08-02'),
+(3, 'Tablet Lite', 'Tablet compacto e leve', 3, 299.99, 'Unidade', 3, '2023-08-03');
